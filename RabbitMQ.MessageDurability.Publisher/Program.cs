ba4082cc2d9e3bcff91ce866259c6f1f4824ccc6
tunahan <tunahancelebi@derinbilgi.com.tr>
2025-01-21 21:07:14 +0300

    Message Durabilty

---
+++ RabbitMQ.MessageDurability.Publisher/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Publisher !");
+
+// 1- RabbitMQ'ya bağlantı oluştur
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://hwwpdpny:btExE1dKV3tfRUrgIy1BAWxyJ4zcksCT@shark.rmq.cloudamqp.com/hwwpdpny");
+
+// 2- Bağlantıyı aktifleştir ve channel aç
+await using IConnection connection = await factory.CreateConnectionAsync();
+IChannel channel = await connection.CreateChannelAsync();
+
+// 3- Queue oluştur
+// exclusive -> false olmalı, channela özel olmaması için.
+// durable -> true olmalı, mesajların kalıcı olması için.
+// eğer bir queue'nun publisher'da durable parametresi true verilmiş ise consumer'dada true olmalıdır.
+await channel.QueueDeclareAsync(queue: "example-queue", exclusive: false, durable: true);
+
+// 4- Queue'ya mesaj gönder
+// RabbitMQ kuyruğa atılan mesajları byte türünden kabul eder. Haliyle göndericeğimiz mesajı byte türüne
+// dönüştürmemiz gerekli.
+byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+await channel.BasicPublishAsync(exchange: "", routingKey: "example-queue", body: message);
+
+var messageProperties = new BasicProperties()
+{
+    Persistent = true // Mesajın kalıcı olması için !!
+};
+
+for (int i = 0; i < 100; i++)
+{
+    byte[] innerMessage = Encoding.UTF8.GetBytes($"Data {i}");
+    await channel.BasicPublishAsync(
+        exchange: "",
+        routingKey: "example-queue",
+        mandatory: true, // Mesajın mutlaka bir kuyruğa ulaşmasını istiyorsanız kullanılır.
+        basicProperties: messageProperties,
+        body: innerMessage);
+}
+
+// mandatory: true kullanıldığında, RabbitMQ'da geri çağırma mekanizmasını (basic.return event)
+// düzgün bir şekilde ele almanız gerekir. Bununla, mesajın neden bir kuyruğa ulaşmadığını
+// öğrenebilirsiniz (örneğin, uygun bir routingKey eşleşmediği için).
+
+// BasicReturn event'ini tanımlıyoruz
+//channel.BasicReturnAsync += async (sender, args) =>
+//{
+//    // Mesajın geri döndüğünde ne yapılacağını belirtiyoruz
+//    Console.WriteLine("Mesaj geri döndü:");
+//    Console.WriteLine($"Exchange: {args.Exchange}");
+//    Console.WriteLine($"Routing Key: {args.RoutingKey}");
+//    Console.WriteLine($"Mesaj: {Encoding.UTF8.GetString(args.Body.ToArray())}");
+//};
+
+Console.Read();
