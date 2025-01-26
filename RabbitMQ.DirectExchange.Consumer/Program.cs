4415746823aabe1b6043c12a99c6a65b4916a1d6
tunahan <tunahancelebi@derinbilgi.com.tr>
2025-01-26 14:14:02 +0300

    Direct Exhange

---
+++ RabbitMQ.DirectExchange.Consumer/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Consumer uygulaması ayakta !");
+
+// Bağlantı Oluştur
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://hwwpdpny:btExE1dKV3tfRUrgIy1BAWxyJ4zcksCT@shark.rmq.cloudamqp.com/hwwpdpny");
+await using IConnection connection = await factory.CreateConnectionAsync();
+IChannel channel = await connection.CreateChannelAsync();
+
+// 1- direct exchange'i publisher'daki ile birebir olucak şekilde channel'a tanımla
+await channel.ExchangeDeclareAsync(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+
+// 2- Queue oluştur
+var queue = await channel.QueueDeclareAsync();
+var queueName = queue.QueueName;
+
+// 3- Queue'yu exchange'e bind et (routing key ile)
+await channel.QueueBindAsync(queue: queueName, exchange: "direct-exchange-example", routingKey: "direct-routing-key");
+
+AsyncEventingBasicConsumer consumer = new(channel);
+await channel.BasicConsumeAsync(queue: queueName, autoAck: true, consumer: consumer);
+
+consumer.ReceivedAsync += async (sender, e) =>
+{
+    var body = e.Body.ToArray();
+    await Task.Delay(100);
+    Console.WriteLine(Encoding.UTF8.GetString(body));
+};
+
+Console.Read();
---
+++ RabbitMQ.DirectExchange.Publisher/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Publisher !");
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://hwwpdpny:btExE1dKV3tfRUrgIy1BAWxyJ4zcksCT@shark.rmq.cloudamqp.com/hwwpdpny");
+await using IConnection connection = await factory.CreateConnectionAsync();
+IChannel channel = await connection.CreateChannelAsync();
+
+// Direct Exchange
+// Mesajların direkt olarak belirli bir kuyruğa gönderilmesini sağlayan exchange tipidir.
+// Her bir mesajın bir routing key'i vardır ve bu routing key'e göre mesajlar ilgili kuyruğa yönlendirilir.
+await channel.ExchangeDeclareAsync(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+
+// mesajları gönder
+while (true)
+{
+    Console.WriteLine("Mesaj giriniz: ");
+    string? message = Console.ReadLine();
+    if (string.IsNullOrEmpty(message))
+    {
+        Console.WriteLine("Mesaj boş olamaz !");
+        continue;
+    }
+
+    // mesajı byte array'e çevir.
+    byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+
+    // routing key belirtilerek mesajın hangi kuyruğa gönderileceği belirlenir
+    await channel.BasicPublishAsync(exchange: "direct-exchange-example", routingKey: "direct-routing-key", body: byteMessage);
+}
